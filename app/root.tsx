66f189265715efb7223bfdaac28e7078392a53a3
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-09-06 17:28:01 -0400

    feat: config prettierc and linter

--- app/root.tsx
+++ app/root.tsx
@@ -14,10 +14,9 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: style }];
 };
 
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Personal token protocol",
   viewport: "width=device-width,initial-scale=1",
 });
 
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,7 @@
 export default function Index() {
   return (
     <div className="p-10">
-      <h1 className="font-bold">
-        Welcome to Remix!
-      </h1>
+      <h1 className="font-bold">Welcome to Remix!</h1>
     </div>
   );
 }
