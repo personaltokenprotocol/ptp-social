aeadb6d53b9bd1165b745db92bad32e8794770d2
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-09-24 13:30:04 -0300

    feat: very first verion of ui for messages

--- app/routes/notifications/$profile/index.tsx
+++ app/routes/notifications/$profile/index.tsx
@@ -1,3 +1,17 @@
 export default function Messages() {
-  return <div className="w-full">Aparece mensaje de mierda</div>;
+  return (
+    <div className="w-full">
+      <div id="message-in" className="p-3">
+        <div className="bg-green-100 rounded-lg p-3 mx-3 inline-block">
+          Hi 👋
+        </div>
+      </div>
+
+      <div id="message-out" className="p-3">
+        <div className="bg-blue-100 rounded-lg p-3 mx-3 inline-block">
+          How are your doing?
+        </div>
+      </div>
+    </div>
+  );
 }
